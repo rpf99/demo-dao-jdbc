8b14ce350144ee7c1e7da357e606bed5e6336566
Rodrigo Franco <rodrigo.pfranco49@edu.unifor.br>
2021-12-27 19:14:14 -0300

    SellerDaoJDBC DaoFactory

--- src/application/program.java
+++ src/application/program.java
@@ -3,6 +3,7 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DaoFactory;
 import model.entities.*;
 
 public class program {
@@ -15,6 +16,9 @@ public class program {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		Seller seller = new Seller(1, "Rodrigo", "rpf34@gmail.com", sdf.parse("19/05/2001"), 3000.00, obj);
+		
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println("\n" + seller);
 	}
