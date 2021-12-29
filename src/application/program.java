a778f9db58e7eb8c54b0193e9be97eeca31db07f
Rodrigo Franco <rodrigo.pfranco49@edu.unifor.br>
2021-12-29 13:35:46 -0300

    UPDATE Seller

--- src/application/program.java
+++ src/application/program.java
@@ -14,31 +14,37 @@ public class program {
 
 		SellerDao sellerdao = DaoFactory.createSellerDao();
 		
-		System.out.println("--------------Teste 1 : Seller By Id--------------\n");
-		Seller se = sellerdao.findById(3);
-		System.out.println(se);
+		//System.out.println("--------------Teste 1 : Seller By Id--------------\n");
+		// Seller se = sellerdao.findById(3);
+		// System.out.println(se);
 		
 		
-		System.out.println("\n\n--------------Teste 2 : Seller Find By DepartmentId--------------\n");
-		Department d = new Department(2,null);
-		List<Seller> list = sellerdao.findByDepartment(d);
-		list.forEach(s -> System.out.println(s) );
+		//System.out.println("\n\n--------------Teste 2 : Seller Find By DepartmentId--------------\n");
+		// Department d = new Department(2,null);
+		// List<Seller> list = sellerdao.findByDepartment(d);
+		// list.forEach(s -> System.out.println(s) );
 		
 		
-		System.out.println("\n\n--------------Teste 3 : Seller Find All--------------\n");
-		list = sellerdao.findAll();
-		list.forEach(s -> System.out.println(s) );
+		//System.out.println("\n\n--------------Teste 3 : Seller Find All--------------\n");
+		// list = sellerdao.findAll();
+		// list.forEach(s -> System.out.println(s) );
 		
 		
-		System.out.println("\n\n--------------Teste 4 : Seller Insert--------------\n");
-		//Date date = new Date();
-		//date.setYear(1985);
+		//System.out.println("\n\n--------------Teste 4 : Seller Insert--------------\n");
+		// Date date = new Date();
+		// date.setYear(1985);
 		
-		//Seller s = new Seller(null, "Greg", "greg@gmail.com", date , 4000.0, d);
+		// Seller s = new Seller(null, "Greg", "greg@gmail.com", date , 4000.0, d);
+		// sellerdao.insert(s);
+		// System.out.println("Inserted: " + s.getId());
 		
-	//	sellerdao.insert(s);
-		//System.out.println("Inserted: " + s.getId());
-		sellerdao.findAll().forEach(s -> System.out.println(s) );
+		
+		System.out.println("\n\n--------------Teste 5 : Seller Update--------------\n");
+		
+		Seller seller = sellerdao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerdao.update(seller);
+		System.out.println("Update Completed");
 	}
 
 }
